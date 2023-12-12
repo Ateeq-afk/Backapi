07dfe7777d86cdf903155c371ae02f61b862467b
unknown <arsalain12@gmail.com>
2023-12-12 18:26:07 +0530

    ettm commit

--- Routes/Book.js
+++ Routes/Book.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const {  checkStatus, initiatepayment } = require('../Controllers/book.js');
+const {  initiatepayment,verifypayment,savepayment } = require('../Controllers/book.js');
 
 const router = express.Router();
 
 
-router.post('/initiatePayment',initiatepayment );
-router.post('/status/:txnId', checkStatus);
-// router.post('/initiatePayment', initiatepayment);
-// router.post('/verifyPayment', verifypayment);
-// router.post('/savePayment', savepayment);
+// router.post('/initiatePayment',initiatepayment );
+// router.post('/status/:txnId', checkStatus);
+router.post('/initiatePayment', initiatepayment);
+router.post('/verifyPayment', verifypayment);
+router.post('/savePayment', savepayment);
 
 module.exports = router;
